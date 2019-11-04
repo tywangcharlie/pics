4194203731c5cc9f25bc3039e2f9f4f4f0116a8b
tywangcharlie <twang220@uic.edu>
2019-11-03 23:48:02 -0600

    Updates

---
+++ precache-manifest.04fb04532c0f1e27c5895b8ee53f1143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f8d0e848c398be9a530458d639cd7b",
+    "url": "/tywangcharlie/pics/index.html"
+  },
+  {
+    "revision": "147667e38f6961bcc147",
+    "url": "/tywangcharlie/pics/static/css/main.064ab282.chunk.css"
+  },
+  {
+    "revision": "393f9bcee8770df83b67",
+    "url": "/tywangcharlie/pics/static/js/2.584ec298.chunk.js"
+  },
+  {
+    "revision": "147667e38f6961bcc147",
+    "url": "/tywangcharlie/pics/static/js/main.a08fa393.chunk.js"
+  },
+  {
+    "revision": "fa602440fe2ceff0510e",
+    "url": "/tywangcharlie/pics/static/js/runtime-main.891328e6.js"
+  }
+]);
