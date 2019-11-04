594abcc46a7e4fcd82c2820002f192d4b9ae3276
tywangcharlie <twang220@uic.edu>
2019-11-03 23:58:32 -0600

    Updates

---
+++ precache-manifest.5e6e2b088b70707c89f32d4c145528e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb39506425e628a18556e3c1878168fb",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "147667e38f6961bcc147",
+    "url": "/pics/static/css/main.064ab282.chunk.css"
+  },
+  {
+    "revision": "393f9bcee8770df83b67",
+    "url": "/pics/static/js/2.584ec298.chunk.js"
+  },
+  {
+    "revision": "147667e38f6961bcc147",
+    "url": "/pics/static/js/main.a08fa393.chunk.js"
+  },
+  {
+    "revision": "9878f3621a56e23127e1",
+    "url": "/pics/static/js/runtime-main.0a93e887.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tywangcharlie/pics/precache-manifest.04fb04532c0f1e27c5895b8ee53f1143.js"
+  "/pics/precache-manifest.5e6e2b088b70707c89f32d4c145528e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tywangcharlie/pics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pics/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
